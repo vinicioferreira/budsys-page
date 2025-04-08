76a25bcdf62cac45a356cb4e98b55aea8c83d407
vinicioferreira <60791294+vinicioferreira@users.noreply.github.com>
2025-04-08 16:42:49 -0300

    Configurando deploy e responsividade para telas menores

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAvT0mxUxx7FXZ5QKmPhTPSnEY4B-O0610",
+    authDomain: "seltcom-4b965.firebaseapp.com",
+    projectId: "seltcom-4b965",
+    storageBucket: "seltcom-4b965.appspot.com",
+    messagingSenderId: "775100171428",
+    appId: "1:775100171428:web:2e3d651eee10c2d680d231",
+    measurementId: "G-5B4M5ECF1W"
+  }
+};
