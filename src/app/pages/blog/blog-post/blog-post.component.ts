d5194cb1fa2473bc484c3812da913495858dd18e
Vinicio Ferreira <vinicio@budsys.com.br>
2026-04-03 20:51:32 -0300

    COnfigurando google analitcs no site

--- src/app/pages/blog/blog-post/blog-post.component.ts
+++ src/app/pages/blog/blog-post/blog-post.component.ts
@@ -4,11 +4,15 @@ import { Observable, switchMap } from 'rxjs';
 import { BlogService } from '../../../services/blog.service';
 import { BlogPost } from '../../../interfaces/blog';
 
+declare var gtag: Function;
+
 @Component({
   selector: 'app-blog-post',
   templateUrl: './blog-post.component.html',
   styleUrls: ['./blog-post.component.scss']
 })
+
+
 export class BlogPostComponent {
   post$: Observable<BlogPost | null>;
 
@@ -16,5 +20,12 @@ export class BlogPostComponent {
     this.post$ = this.route.paramMap.pipe(
       switchMap(params => this.blog.getPostBySlug(params.get('slug') || ''))
     );
+  }
+
+  trackWhats(slug: string) {
+    gtag('event', 'click_whatsapp', {
+      event_category: 'engajamento',
+      event_label: slug
+    });
   }
 }
